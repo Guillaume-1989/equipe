7c4b2f8f147c8aefc601cfb24d9e3a7c4827bce1
kevin oddi <kevinoddi.deyris@gmail.com>
2020-07-30 17:02:53 +0200

    ajout de l'anectode de kevin

--- anecdotes.php
+++ anecdotes.php
@@ -6,5 +6,6 @@ require('header.php'); ?>
 <p>Quand vous pensez à votre cerveau, votre cerveau pense à lui-même.</p>
 
 <p>annecdote de jc: il fait beau aujourd'hui j'ai envie d'aller à la plage.</p>
+<p> annecdote de kevin: il ma fait galeré </p>
 
 <?php require('footer.php'); ?>
