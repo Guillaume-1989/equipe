da391045e4003467d47296b1a43cc1fd53ad52a5
jeanchristoph <64985971+jeanchristoph@users.noreply.github.com>
2020-07-30 16:51:43 +0200

    ajout header et footer

--- jc.php
+++ jc.php
@@ -1,3 +1,7 @@
-Bonjour nouveau membre Je m'applle JC 
+<?php
+require('header.php'); ?>
+<p>Bonjour nouveau membre Je m'applle JC 
 Je viens de la planete mars. 
-Je suis né il y a pas tres longtemps mais quand meme.
+Je suis né il y a pas tres longtemps mais quand meme.</p>
+
+<?php require('footer.php'); ?>
