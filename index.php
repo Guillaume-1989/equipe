728262b7bdb05e4f041d55adc0a40922b2fce316
Guillaume LOPEZ <lopezguillaume84@gmail.com>
2020-07-30 16:16:53 +0200

    La grosse équipe V3

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Bienvenue sur le site de La grosse équipe !
-</body>
-</html>
+<?php
+require('header.php'); ?>
+<h2>Bienvenue</h2>
+<p>Bienvenue sur le site de l'équipe <?=NOM_DE_LEQUIPE?></p>
+<?php require('footer.php'); ?>
