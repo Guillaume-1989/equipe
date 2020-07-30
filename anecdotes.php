1fb1521b7fe914057debb5b0010105569aafd7f2
Guillaume LOPEZ <lopezguillaume84@gmail.com>
2020-07-30 17:03:48 +0200

    ok

--- anecdotes.php
+++ anecdotes.php
@@ -1,11 +1,12 @@
 <?php
 require('header.php'); ?>
 
-<h2>Petites annecdotes</h2>
+<h2>Petites anecdotes</h2>
 
 <p>Quand vous pensez à votre cerveau, votre cerveau pense à lui-même.</p>
 
 <p>annecdote de jc: il fait beau aujourd'hui j'ai envie d'aller à la plage.</p>
+
 <p> annecdote de kevin: il ma fait galeré </p>
 
 <?php require('footer.php'); ?>
